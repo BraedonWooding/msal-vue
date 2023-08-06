08393e2a73ce4e97b9b5e67e19f733dcd24b6554
Braedon Wooding <braedonww@gmail.com>
2023-08-06 17:39:19 +1000

    Support vue2/3 typing

--- lib/plugin.ts
+++ lib/plugin.ts
@@ -1,24 +1,29 @@
-import { Configuration } from '@azure/msal-browser';
-import { MsalLayer } from './main';
-import { isVue2, isVue3 } from 'vue-demi'
+import { Configuration } from "@azure/msal-browser";
+import { MsalLayer } from "./main";
+import { isVue2, isVue3 } from "vue-demi";
 
-declare module 'vue/types/vue' {
-    interface Vue {
-        $msal: MsalLayer,
-    }
+declare module "@vue/types/vue" {
+  interface Vue {
+    $msal: MsalLayer;
+  }
+}
+declare module "vue" {
+  interface ComponentCustomProperties {
+    $msal: MsalLayer;
+  }
 }
 
 class MsalPlugin {
-    // to annoying to type Vue since it's supporting both vue2 and vue
-    static install(Vue: any, options: Configuration): void {
-        if (isVue2) {
-            Vue.prototype.$msal = new MsalLayer(options);
-        } else if (isVue3) {
-            Vue.config.globalProperties.$msal = new MsalLayer(options);
-        } else {
-            throw "Should either be vue2 or vue3, can't detect either";
-        }
+  // to annoying to type Vue since it's supporting both vue2 and vue
+  static install(Vue: any, options: Configuration): void {
+    if (isVue2) {
+      Vue.prototype.$msal = new MsalLayer(options);
+    } else if (isVue3) {
+      Vue.config.globalProperties.$msal = new MsalLayer(options);
+    } else {
+      throw "Should either be vue2 or vue3, can't detect either";
     }
+  }
 }
 
-export {MsalPlugin, MsalLayer, Configuration};
+export { MsalPlugin, MsalLayer, Configuration };
